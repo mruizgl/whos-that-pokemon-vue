b7b9b814650291c526944d75a98a9261f56ecb44
Melissa Ruiz <125923092+mruizgl@users.noreply.github.com>
2025-01-26 22:47:47 +0000

    feat(reto5): se avanza en reto 5

--- vue-project/src/modules/composables/usePokemonGame.ts
+++ vue-project/src/modules/composables/usePokemonGame.ts
@@ -1,22 +1,32 @@
 import { onMounted, ref } from 'vue';
 import { GameStatus } from '../pokemon/interfaces/game-status.enum';
 import { pokemonApi } from '../pokemon/api/pokemonApi';
+import type {Pokemon} from '../pokemon/interfaces/pokemon.interface';
+import type { PokemonListResponse } from '../pokemon/interfaces/pokemon-list-response';
 
 export const usePokemonGame = () => {
     const gameStatus = ref<GameStatus>(GameStatus.Playing);
 
-    const getPokemons = async () => {
-        try {
-            const response = await pokemonApi.get('/?limit=151');
+    const getPokemons = async (): Promise<Pokemon[]> => {
+        const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
 
-            console.log(response.data);
-        } catch (error) {
-            console.error('Error fetching Pokémon data:', error);
-        }
-    };
+        const pokemonsArray = response.data.results.map( pokemon => {
+            const urlParts = pokemon.url.split('/');
+            const id = urlParts[urlParts.length - 2] ?? 0;
+            return {
+                name: pokemon.name,
+                id: +id,
+            }
+        });
 
-    onMounted(() => {
-        getPokemons();
+        return pokemonsArray;
+    }
+
+    onMounted(async() => {
+        const pokemons = await getPokemons();
+
+        console.log(pokemons);
+        
     });
 
     return {
--- vue-project/src/modules/pokemon/api/pokemonApi.ts
+++ vue-project/src/modules/pokemon/api/pokemonApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import type { AxiosResponse } from "axios";
+import type  { PokemonListResponse } from "../interfaces/pokemon-list-response"; 
 
 export class pokemonApi {
-    static async get(limit: string) {
-        const response = await axios.get(
+    static async get<T>(limit: string): Promise<AxiosResponse<T>> {  // Añadir tipo genérico <T>
+        const response = await axios.get<T>(
             'https://pokeapi.co/api/v2/pokemon' + limit + '&offset=0'
         );
         return response;
